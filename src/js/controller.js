3a8197fee19fd2eff5c66da72c4b8b247209e0dd
NikDoe <nikd0e@yandex.ru>
2021-07-29 19:01:24 +0300

    feature (Forkify) : loading a recipe from api

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,1 +1,43 @@
-console.log(1);
+const recipeContainer = document.querySelector('.recipe');
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc76'
+    );
+    const data = await res.json();
+
+    let { recipe } = data.data;
+
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+
+    console.log(recipe);
+
+    if (!res.ok) throw new Error(`${data.message} \nstatus : (${res.status})`);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRecipe();
