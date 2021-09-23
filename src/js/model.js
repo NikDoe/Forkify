017727204a0b183efe325581670a94fa4737fab0
NikDoe <nikd0e@yandex.ru>
2021-09-23 17:59:51 +0300

    feature (Forkify) : add config.js file

--- src/js/model.js
+++ src/js/model.js
@@ -1,4 +1,5 @@
 import 'regenerator-runtime/runtime'
+import { API_URL } from './config';
 
 export const  state = {
   recipe: {},
@@ -7,7 +8,7 @@ export const  state = {
 export const loadRecipe = async function(id) {
   try{
     const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+      `${API_URL}/${id}`
     );
     const data = await res.json();
 
