28ffb2e277414a27da4fbcf93a83b8b339b3407e
NikDoe <nikd0e@yandex.ru>
2021-09-27 21:17:50 +0300

    refactor (Forkify) : add _generateMarkupButton method

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -20,47 +20,34 @@ class PaginationView extends View{
 
     //page 1, and there are other pages
     if(currPage === 1 && numPages > 1) {
-      return `
-        <button data-goto='${currPage + 1}' class="btn--inline pagination__btn--next">
-          <span>Page ${currPage + 1}</span>
-          <svg class="search__icon">
-            <use href="${icons}#icon-arrow-right"></use>
-          </svg>
-        </button>
-      `
+      return this._generateMarkupButton(currPage + 1, 'next', 'right');
     }
 
     //last page
     if(currPage === numPages && numPages > 1) {
-      return `
-        <button data-goto='${currPage - 1}' class="btn--inline pagination__btn--prev">
-          <svg class="search__icon">
-            <use href="${icons}#icon-arrow-left"></use>
-          </svg>
-          <span>Page ${currPage - 1}</span>
-        </button>
-      `
+      return this._generateMarkupButton(currPage - 1, 'prev', 'left');
     }
 
     //other pages
     if(currPage < numPages) {
       return `
-        <button data-goto='${currPage - 1}' class="btn--inline pagination__btn--prev">
-          <svg class="search__icon">
-            <use href="${icons}#icon-arrow-left"></use>
-          </svg>
-          <span>Page ${currPage - 1}</span>
-        </button>
-        <button data-goto='${currPage + 1}' class="btn--inline pagination__btn--next">
-          <span>Page ${currPage + 1}</span>
-          <svg class="search__icon">
-            <use href="${icons}#icon-arrow-right"></use>
-          </svg>
-        </button>
+        ${this._generateMarkupButton(currPage - 1, 'prev', 'left')}
+        ${this._generateMarkupButton(currPage + 1, 'next', 'right')}
       `
     }
     //page 1, and there are NO other pages
     return '';
+  }
+
+  _generateMarkupButton(num, str, dir) {
+    return `
+      <button data-goto='${num}' class="btn--inline pagination__btn--${str}">
+        <svg class="search__icon">
+          <use href="${icons}#icon-arrow-${dir}"></use>
+        </svg>
+        <span>Page ${num}</span>
+      </button>
+    `
   }
 }
 
